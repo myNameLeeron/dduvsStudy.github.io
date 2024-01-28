5cf6b9764bfb831272dde872d5084d4b20968bb6
myNameLeeron <157977769+myNameLeeron@users.noreply.github.com>
2024-01-28 18:57:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// app.js
 document.getElementById('search').addEventListener('input', function(e) {
     console.log('Поиск: ' + e.target.value);
+});
+
+document.getElementById('menuBtn').addEventListener('click', function() {
+    document.getElementById('menu').style.width = '250px';
+});
+
+document.querySelector('.closeBtn').addEventListener('click', function() {
+    document.getElementById('menu').style.width = '0';
 });
